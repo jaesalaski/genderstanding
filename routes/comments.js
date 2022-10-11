e3965504134b319054319030ce6ed1990cf0a260
codeBy.J <codebyj@gmail.com>
2022-10-11 18:43:54 -0500

    functional nested loop with comments and posts

---
+++ routes/comments.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const commentsController = require("../controllers/comments");
+const postsController = require("../controllers/posts");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Commment Routes - simplified for now
+
+// router.get("/:id", ensureAuth, postsController.getProfile);
+
+router.post("/createComment/:id", commentsController.createComment);
+
+// router.put("/likeComment/:id", commentsController.likeComment);
+
+// router.delete("/deleteComment/:id", commentsController.deleteComment);
+
+module.exports = router;
