e48f995952849c53f364205275e11e2c46fc6ce9
J Salaski <hello@jsalaski.com>
2022-10-17 13:53:42 -0500

    comment likes and post deletes on profile.ejs

--- controllers/comments.js
+++ controllers/comments.js
@@ -15,30 +15,39 @@ module.exports = {
       console.log(err);
     }
   },
-  // getComment : async (req, res) => {
-  //   try {
-  //     const posts = await Post.find({ user: req.user.id });
-  //     const comments = await Comment.find({ post: req.params.id }).sort({ createdAt: "desc" }).lean();
-  //     res.render("profile.ejs", { posts: posts, user: req.user, comments: comments });
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // },
+
+  likeComment: async (req, res) => {
+    try {
+      await Comment.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Likes +1");
+      res.redirect(`/profile`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
 }
-//   likePost: async (req, res) => {
-//     try {
-//       await Post.findOneAndUpdate(
-//         { _id: req.params.id },
-//         {
-//           $inc: { likes: 1 },
-//         }
-//       );
-//       console.log("Likes +1");
-//       res.redirect(`/post/${req.params.id}`);
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   },
+
+// likePost: async (req, res) => {
+//   try {
+//     await Post.findOneAndUpdate(
+//       { _id: req.params.id },
+//       {
+//         $inc: { likes: 1 },
+//       }
+//     );
+//     console.log("Likes +1");
+//     res.redirect(`/post/${req.params.id}`);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// },
+
+
 //   deletePost: async (req, res) => {
 //     try {
 //       // Find post by id
@@ -52,3 +61,12 @@ module.exports = {
 //     } catch (err) {
 //       res.redirect("/profile");
 
+  // getComment : async (req, res) => {
+  //   try {
+  //     const posts = await Post.find({ user: req.user.id });
+  //     const comments = await Comment.find({ post: req.params.id }).sort({ createdAt: "desc" }).lean();
+  //     res.render("profile.ejs", { posts: posts, user: req.user, comments: comments });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // },
--- routes/comments.js
+++ routes/comments.js
@@ -4,13 +4,13 @@ const commentsController = require("../controllers/comments");
 const postsController = require("../controllers/posts");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-//Commment Routes - simplified for now
+//Commment Routes
 
 // router.get("/:id", ensureAuth, postsController.getProfile);
 
 router.post("/createComment/:id", commentsController.createComment);
 
-// router.put("/likeComment/:id", commentsController.likeComment);
+router.put("/likeComment/:id", commentsController.likeComment);
 
 // router.delete("/deleteComment/:id", commentsController.deleteComment);
 
